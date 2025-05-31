d3ccefde10c570a07ddde7dfe827d081ab8c85cf
Jose Manuel <josemsanchezg511@gmail.com>
2025-05-31 17:14:52 -0500

    condicionales

---
+++ Condicionales.py
@@ -0,0 +1,56 @@
+"""¿QUÉ ES EL FLUJO? El flujo es una forma de entender la sucesion de las instrucciones de un programa, estas instrucciones se ejecutan una depues de otra de forma ordenada y suelen tener el objetivo final de manipular informacion."""
+
+# print("Ejemplo N° [1]")
+# edad = int(input("Ingrese su edad: "))
+# if edad >=18:
+#     print("Eres mayor de edad.")
+# else:
+#     print("No eres mayor de edad.")
+
+# print("Ejemplo N° [2]")
+# a = float(input("Ingresa un numero: "))
+# b = float(input("Ingresa otro número: "))
+# c = float(input("Ingresa un último número: "))
+# if a > b and c > a:
+#     print("Ambas condiciones son verdaderas")
+# else: 
+#     print("Una de las condiciones es falsa")
+    
+# print("Ejemplo N° [3]")
+# numero = float(input("Ingrese un número: "))
+# if numero > 36:
+#     print("El numero es grande.")
+# else:
+#     print("El número es pequeño.")
+    
+# print("Ejemplo N° [4]")
+# x = int(input("Ingrese un número: "))
+# if x >10:
+#     print("El número está por encima de 10")
+#     if x >20:
+#         print("El número está por encima de 20")
+#         if x >30:
+#             print("Y tambien por encima de 30")
+# else:
+#     print("Está debajo de 10")
+    
+# print("Ejemplo N° [4] Elif")
+
+# num1 = int(input("Ingrese un número: "))
+# num2 = int(input("Ingrese otro número: "))
+# result = num1 * num2
+# print(f"El resultado es: {result}")
+
+# if result <=99:
+#     print("El resultado es menor o igual a 99")
+# elif result >=100:
+#     print("El resultado es mayor o igual a 100")
+# elif result >=1000:
+#     print("El resultado es mayor a 1000")
+# elif result >=100000:
+#     print("El resultado es mayor o igual a 100000")
+# else:
+#     print("El número ingresado no es válido o coherente.")
+            
+
+    
