bc0ae8f41354eb23c1f8cf81e813c808499fbf86
Jose Manuel <josemsanchezg511@gmail.com>
2025-06-07 15:44:56 -0500

    condicionales

--- Taller_Condicionales.py
+++ Taller_Condicionales.py
@@ -2,6 +2,7 @@
 
 # print("EJERCICIOS CON CONDICIONALES Y OPERACIONES MATEMÁTICAS")
 
+# print("------------------------------------------------------")
 # print("EJERCICIO N° [1]:")
 # num1 = float(input("Ingrese un número: "))
 # if num1 ==0:
@@ -12,7 +13,7 @@
 #     print("Este número es negativo.")
 # else:
 #     print("Dato incorrecto o letra.")
-
+# print("------------------------------------------------------")
 # print("EJERCICIO N° [2]:")
 # num2 = int(input("Ingrese un número: "))
 # num3 = int(input("Ingrese un segundo número: "))
@@ -24,7 +25,7 @@
 #     print(f"el número {num2} y el número {num3} son iguales ")
 # else:
 #     print("Ingresa un dato correcto.")
-    
+# print("------------------------------------------------------")
 # print("EJERCICIO N° [3]:")
 # num4 = int(input("Ingresa un número: "))
 # mitad = num4 // 2 
@@ -35,7 +36,7 @@
 #     print("El número es impar")
 # else:
 #     print("Ingresa un dato correcto")
-
+# print("------------------------------------------------------")
 # print("EJERCICIO N° [4]:")
 # num5 = int(input("Ingresa un número: "))
 # if num5 >= 10 and num5 <= 20:
@@ -44,7 +45,7 @@
 #     print("El número no está entre 10 y 20")
 # else:
 #     print("Ingresa un dato correcto")
-
+# print("------------------------------------------------------")
 # print("EJERCICIO N° [5]:")
 # num6 = int(input("Ingresa un número: "))
 # num7 = int(input("Ingresa otro número: "))
@@ -59,8 +60,8 @@
 #     print("Todos los números son iguales")
 # else:
 #     print("Ingresa un dato correcto")
-
-# print("Ejemplo N° [6]:")
+# print("------------------------------------------------------")
+# print("EJERCICIO N° [6]:")
 # num9 = float(input("Ingresa el precio $ del producto: "))
 # if num9 > 100:
 #     descuento = num9 * 0.10
@@ -70,8 +71,8 @@
 #     print("No se aplica un descuento")
 # else: 
 #     print("Ingresa un dato correcto")
-
-# print("Ejemplo N° [7]:")
+# print("------------------------------------------------------")
+# print("EJERCICIO N° [7]:")
 # edad = int(input("Ingresa tu edad: "))
 # if edad >=18 and edad <=120:
 #     print("Usted puede votar")
@@ -79,8 +80,8 @@
 #     print("Usted no puede votar")
 # else:
 #     print("Ingresa un dato o edad lógica")
-
-# print("Ejemplo N° [8]:")
+# print("------------------------------------------------------")
+# print("EJERCICIO N° [8]:")
 # precio1 = float(input("Ingresa un precio $: "))
 # tipo_cliente = input("Ingresa que tipo de cliente eres; VIP o normal: ")
 # if tipo_cliente == "normal":
@@ -91,15 +92,15 @@
 #     print(f"Por ser VIP se te aplica un descuento del 20%. Tu precio con el descuento es {precio_final2}$")
 # else:
 #     print("Escribe de forma correcta el tipo de cliente que eres o dato incorrecto")
-
-# print("Ejemplo N° [9]:")
+# print("------------------------------------------------------")
+# print("EJERCICIO N° [9]:")
 # num10 = int(input("Ingresa un número: "))
 # if num10 % 5 == 0 and num10 % 3 == 0:
 #     print("Es múltiplo de 5 y 3")
 # else:
 #     print("No es múltiplo de ambos")
-
-# print("Ejemplo N° [10]:")
+# print("------------------------------------------------------")
+# print("EJERCICIO N° [10]:")
 # numero1 = int(input("Ingresa un número: "))
 # divisor1 = int(input("Ingresa un divisor: "))
 # divisor2 = int(input("Ingresa otro divisor: "))
@@ -110,8 +111,8 @@
 #     print(f"El número {numero1} No es divisible entre {divisor1} y {divisor2}")
 
 # print("EJERCICIOS CON LISTAS (CON CONDICIONALES)")
-
-# print("Ejemplo N° [11]:")
+# print("------------------------------------------------------")
+# print("EJERCICIO N° [11]:")
 # lista1 = [int(input("Ingresa un número: ")), int(input("Ingresa un segundo número: ")), int(input("Ingresa un tercer número: ")), int(input("Ingresa un cuarto número: ")), int(input("Ingresa un quinto número: "))]
 # if lista1[2] > 10:
 #     print(f"El tercer número {lista1[2]} es mayor a 10")
@@ -121,15 +122,15 @@
 #     print(f"El tercer número {lista1[2]} es igual a 10")
 # else:
 #     print("Dato incorrecto")
-
-# print("Ejemplo N° [12]:")
+# print("------------------------------------------------------")
+# print("EJERCICIO N° [12]:")
 # lista2 = [3, 5, 7, 9]
 # if lista2[2]:
 #     print("El número 7 se encuentra en la lista")
 # else: 
 #     print("El número 7 no está en la lista")
-
-# # print("Ejemplo N° [13]:")
+# print("------------------------------------------------------")
+# # print("EJERCICIO N° [13]:")
 # lista3 = [4, 6, 2, 8]
 # suma = lista3[0] + lista3[1]
 # if suma >10:
@@ -140,8 +141,8 @@
 #     print("La suma es igual a 10")
 # else:
 #     print("Dato incorrecto")
-
-# print("Ejemplo N° [14]:")
+# print("------------------------------------------------------")
+# print("EJERCICIO N° [14]:")
 # lista4 = ["Ana", "Luis", "Pedro", "Marta"]
 # print(f"El último nombre es {lista4[-1]}")
 
@@ -151,8 +152,8 @@
 #     print("Nombre diferente")
 # else:
 #     print("Dato incorrecto")
-    
-# print("Ejemplo N° [15]:")
+# print("------------------------------------------------------")
+# print("EJERCICIO N° [15]:")
 # lista5 = [input("Ingresa el primer color: ").lower(), input("Ingresa el segundo color: ").lower(), input("Ingresa el tercer color: ").lower()]
 
 # if lista5[1] == "azul":
@@ -162,7 +163,8 @@
 #     print("El segundo color no es azul, no requiere cambios")
 # else:
 #     print("Ingresa un dato correcto")
+# print("------------------------------------------------------")
 
 # print("EJERCICIOS CON TUPLAS (CON CONDICIONALES)")
 
-print("Ejemplo N° [16]:")
+# print("EJERCICIO N° [16]:")
