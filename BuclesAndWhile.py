9ed5ea6c7d2d853e4ff1b153280461c86c305ac2
Jose Manuel <josemsanchezg511@gmail.com>
2025-06-14 17:43:46 -0500

    bucle

--- BuclesAndWhile.py
+++ BuclesAndWhile.py
@@ -1,7 +1,13 @@
-contador = 1
-while contador <= 100:
-    print("contador", contador)
-    contador -= 1
+# contador = 1
+# while contador <= 100:
+#     print("contador", contador)
+#     contador -= 1
 
+print("--------------------------------")
 
-    
+while True:
+    texto = input("Escribe algo (o escribe salir para terminar): ")
+    if texto == "salir":
+        break
+    print(f"Escribiste {texto}")
+# break detiene inmediatamente un ciclo (while o for), aunque la condición del ciclo siga siendo verdadera
