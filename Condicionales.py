d54e552525258620441771375c1da105853addfd
Jose Manuel <josemsanchezg511@gmail.com>
2025-06-21 20:29:32 -0500

    t

--- Condicionales.py
+++ Condicionales.py
@@ -104,68 +104,86 @@
 # else:
 #     print(f"El resultado {result} es relativamente pequeño")
 
-print("----------------------------------------------------")
+# print("----------------------------------------------------")
 
-print("Ejemplo N° [6]")
+# print("Ejemplo N° [6]")
 
-# solicito dos números enteros al usuario
-nume = int(input("Ingrese un número: "))
-nume2 = int(input("Ingrese otro número: "))
+# # solicito dos números enteros al usuario
+# nume = int(input("Ingrese un número: "))
+# nume2 = int(input("Ingrese otro número: "))
 
-result1 = nume / nume2
+# result1 = nume * nume2
 
-
-if result1 <=99:
-    print(f"El resultado {result1} es menor o igual a 99")
-elif result1 >=100:
-    print(f"El resultado {result1} es mayor o igual a 100")
-elif result1 >=1000:
-    print(f"El resultado {result1} es mayor a 1000")
-elif result1 >=100000:
-    print(f"El resultado {result1} es mayor o igual a 100000")
-else:
-    print("El número ingresado no es válido o coherente.")
+# # evalúa el resultado y muestra un mensaje según su valor
+# if result1 >=100000:
+#     print(f"El resultado {result1} es mayor o igual a 100000")
+# elif result1 >=1000:
+#     print(f"El resultado {result1} es mayor a 1000")
+# elif result1 >=100:
+#     print(f"El resultado {result1} es mayor o igual a 100")
+# elif result1 <=99:
+#     print(f"El resultado {result1} es menor o igual a 99")
+# else:
+#     print("El número ingresado no es válido o coherente.")
     
-print("----------------------------------------------------")
-print("Ejemplo N° [7]")
-fecha = int(input("Ingrese su año de naciminento: "))
+# print("----------------------------------------------------")
 
-if fecha >=1920 and fecha <=1940:
-    print("perteneces a la generación Silenciosa")
-elif fecha >=1946 and fecha <=1964:
-    print("Perteneces a la generación Baby Boomer")
-elif fecha >=1965 and fecha <=1979:
-    print("Perteneces a la generación X")
-elif fecha >=1980 and fecha <=2000:
-    print("Perteneces a la generación Y")
-elif fecha >=2001 and fecha <=2010:
-    print("Perteneces a la generación Z")
-else:
-    print("Ingresa un año correcto o coherente")
-print("----------------------------------------------------")
-print("Ejemplo N° [8]: PROGRAMA DE CRÉDITO BANCARIO.")
+# print("Ejemplo N° [7]")
 
-nombre = input("Ingrese su nombre: ")
-edad = int(input("Ingrese su edad: "))
-if edad >=18:
-    print("Eres mayor de edad, puedes pedir tu crédito bancario.")
-elif edad <=17:
-    print("No eres mayor de edad.")
-else:
-    print("Ingresa un dato correcto o coherente.")
-print("----------------------------------------------------")
+# # solicito el nombre y el año de nacimiento del usuario
+# nombre2 = input("Ingrese su nombre: ")
+# fecha = int(input("Ingrese su año de naciminento: "))
+
+# # evalúa la fecha dependiendo del año de nacimiento del usuario
+# if fecha >=1920 and fecha <=1940:
+#     print(f"{nombre2}, Perteneces a la generación Silenciosa")
+# elif fecha >=1946 and fecha <=1964:
+#     print(f"{nombre2}, Perteneces a la generación Baby Boomer")
+# elif fecha >=1965 and fecha <=1979:
+#     print(f"{nombre2}, Perteneces a la generación X")
+# elif fecha >=1980 and fecha <=2000:
+#     print(f"{nombre2}, Perteneces a la generación Y")
+# elif fecha >=2001 and fecha <=2010:
+#     print(f"{nombre2}, Perteneces a la generación Z")
+# else:
+#     print("Ingresa un año correcto o coherente")
+    
+# print("----------------------------------------------------")
+
+# print("Ejemplo N° [8]: MINI PROGRAMA DE CRÉDITO BANCARIO.")
+
+# # solicito el nombre y edad al usuario
+# nombre3 = input("Ingrese su nombre: ")
+# edad = int(input("Ingrese su edad: "))
+
+# # verificar que la edad sea coherente
+# if edad <0 or edad >130:
+#     print("Ingrese una edad válida")
+# else:
+#     # evaluación de la mayoría de edad
+#     if edad >=18:
+#         print(f"{nombre3}, eres mayor de edad, puedes pedir tu crédito bancario")
+#     else:
+#         print(f"{nombre3}, no eres mayor de edad, no puedes accerder al crédito")
+    
+# print("----------------------------------------------------")
+
 # print("Ejemplo N° [9]: PROGRAMA DE SALA DE JUEGOS.")
 
+# # pide al usuario la edad
 # edad = int(input("Ingrese la edad: "))
+
+# # validacion del rango de la edad y calcúlo del precio 
 # if edad ==0 and edad <=4:
 #     print("Entra gratis.")
 # elif edad ==5 and edad <=18:
-#     print("Paga 5 Euros.")
+#     print("Paga 5 Euros.") 
 # elif edad >=19:
 #     print("Paga 18 Euros.")
 # else:
-#     print("Ingresaste un dato incorrecto.")
+#     print("Ingresaste un dato incorrecto.") 
 
+# print("----------------------------------------------------")
 
 
 
