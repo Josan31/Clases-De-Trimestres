bc134a0e2d31e426b9b417e3776540bda53a6297
Jose Manuel <josemsanchezg511@gmail.com>
2025-06-29 11:19:32 -0500

    Métodos

--- Clases De Python/Metodos.py
+++ Clases De Python/Metodos.py
@@ -32,53 +32,60 @@
 # print(f"La palabra '{palabras[1]}' tiene {len(palabras[1])} letras")
 # print(f"La palabra '{palabras[2]}' tiene {len(palabras[2])} letras")
 
+# print("=" * 60)
+
+# print("==== MÉTODO N°2: .FIND() ====")
+
+# print("\n- .find(): buscar una subcadena\n".upper())
+
+# print(".find() Devuelve la posición (índice) donde aparece por primera vez una letra o palabra. Si no se encuentra, devuelve -1.")
+
+# print("-" * 50)
+
+# print("==== EJEMPLO N°1 ====")
+
+# # Pide los datos al usuario
+# frase = input("\nIngresa una frase: ").lower()
+# letra = input("Ingrese una sola letra (ej: a): ").lower()
+
+# # si la letra ingresada por el usuario no está en la frase
+# if letra not in frase:
+#     print(f"\nLa letra '{letra}' NO está en la frase: '{frase}'. Por lo cual arroja un {frase.find(letra)} ")
+    
+# # Si la letra ingresada por el usuario sí está en la frase
+# else:
+#     posicion = frase.find(letra)
+#     print(f"\nLa letra '{letra}' SI está en la frase '{frase}'")
+#     print(f"Aparece por primera vez en la posición {posicion}")
+
+# print("-" * 50)
+
+# print("==== EJEMPLO N°2 ====")
+
+# # Pide al usuario la frase y la palabra a buscar
+# frase2 = input("\nEscribe una frase misteriosa: ").lower()
+# clave = input("\nIngrese la palabra clave para buscar: ").lower()
+
+# # Verifica si la palabra ingresada está en la frase
+# pos = frase2.find(clave)
+
+# # Si la palabra ingresada por el usuario no está en la frase
+# if pos == -1:
+#     print("\nLa palabra clave no fue encontrada... sigue buscando")
+    
+# # Si sí está, arroja un mensaje 
+# else:
+#     print(f"\n¡Palabra encontrada! Empieza en la posición {pos}")
+
 print("=" * 60)
 
-print("Método N° [2]".upper())
-print("\n- .find(): buscar una subcadena\n".upper())
+print("==== MÉTODO N°3: EXTRACCIÓN ====")  
 
-print(".find() Devuelve la posición (índice) donde aparece por primera vez una letra o palabra. Si no se encuentra, devuelve -1.")
 
-print("-------------------------------------------")
 
-print("\nEjemplo N° [1]".upper())
 
-# Pide los datos al usuario
-frase = input("Ingresa una frase: ").lower()
-letra = input("Ingrese una sola letra (ej: a): ").lower()
 
-# si la letra ingresada por el usuario no está en la frase
-if letra not in frase:
-    print(f"\nLa letra '{letra}' NO está en la frase: '{frase}'. Por lo cual arroja un {frase.find(letra)} ")
-    
-# Si la letra ingresada por el usuario sí está en la frase
-else:
-    posicion = frase.find(letra)
-    print(f"La letra '{letra}' SI está en la frase '{frase}'")
-    print(f"Aparece por primera vez en la posición {posicion}")
 
-print("-------------------------------------------")
-
-print("\nEjemplo N° [2]".upper())
-
-# Pide al usuario la frase y la palabra a buscar
-frase2 = input("Escribe una frase misteriosa (sin dejar espacios entre ellas): ").lower()
-clave = input("Ingrese la palabra clave para buscar: ").lower()
-
-# Verifica si la palabra ingresada está en la frase
-pos = frase2.find(clave)
-
-# Si la palabra ingresada por el usuario no está en la frase
-if pos == -1:
-    print("La palabra clave no fue encontrada... sigue buscando")
-    
-# Si sí está, arroja un mensaje 
-else:
-    print(f"¡Palabra encontrada! Empieza en la posición {pos}")
-
-# print("=======================================================")
-
-# print("Método N° [3]".upper())   
 
 # print("\n- len(): Longitud de una cadena (o lista)\n".upper())
   
